9a9ff7f65cd7e01f112d58e4bae6ea54945a9a7b
Sylvain Gubian <sylvain.gubian@pmi.com>
2018-11-19 00:31:00 +0100

    Fixed documentation, fix 3dots parameter transmission

--- R/plot.R
+++ R/plot.R
@@ -5,12 +5,15 @@
 
 NPA$unlock()
 
+setMethod("plot",
+          signature(x = "NPA", y = "character"),
+          function(x, y=c('heatmap', 'graph', 'graphjs'), ...) plot.NPA(x, y, ...))
+
 #==============================================================================
 # Method: plot for NPA object
 #==============================================================================
 #' Generate a NPA plot showing either a NPA summary heatmap or the network
 #' graph with NPA scores depending of the type of type of plot
-#' @exportMethod plot
 #' @param x NPA R6 class instance
 #' @param type An character vector. Type of plot. If \code{type} is "heatmap", default version
 #' showing a summary heatmap with leading nodes scores for the different
@@ -22,22 +25,21 @@ NPA$unlock()
 #' @include drawNPAjs.R
 #' @include NPAheatmap.R
 #' @include drawNPAmodule.R
-setMethod("plot",
-          signature(x = "NPA", y = "character"),
-          function(x, y=c('heatmap', 'graph', 'graphjs'), ...) plot.NPA(x, y, ...))
-
-plot.NPA <- function(x, y, ...) {
+#' @rdname plot.NPA
+#' @method plot NPA
+#' @export
+plot.NPA <- function(x, y="heatmap", ...) {
     x$plot(y, ...)
 }
 
 NPA$set("public", "plot", function(type=c('heatmap', 'graph', 'graphjs'), ...) {
     type <- match.arg(type)
     if (type=='heatmap') {
-        return(invisible(heatmapNPA(private$data)))
+        return(invisible(heatmapNPA(private$data, ...)))
     } else if (type == 'graph') {
-      return(invisible(drawNPAmodule(private$data)))
+      return(invisible(drawNPAmodule(private$data, ...)))
     } else {
-      return(invisible(drawNPAjs(private$data)))
+      return(invisible(drawNPAjs(private$data, ...)))
     }
 })
 
@@ -45,32 +47,37 @@ NPA$lock()
 
 
 BIF$unlock()
+setMethod("plot",
+          signature(x = "BIF", y = "integer"),
+          function(x, y = 1 , ...) {
+            plot.BIF(x, y = 1, ...)
+          })
+
 #===============================================================================
 # Method: plot for BIF object
 #===============================================================================
 #' Generate a BIF plot showing starplots per comparisons or per network families
-#' @exportMethod plot
 #' @param x BIF R6 class instance
 #' @param type An integer value. Type of plot. `type` is 1, default version
 #' showing network contributions for each comparison. If `type` is 2, startplots
 #' figures of contributions of comparisons per network families is shown.
-#' @param ... Additional parameters that can be passed to startplot functions.
+#' @param ... Additional parameters that can be passed to startplot functions (see
+#' startplotBIFcontrasts and starplotBIFnetworks for details).
 #' @docType methods
 #' @include starplotBIFcontrasts.R
 #' @include starplotBIFnetworks.R
-setMethod("plot",
-          signature(x = "BIF", y = "integer"),
-          function(x, y = 1 , ...) plot.BIF(x, y = 1, ...))
-
+#' @rdname plot.BIF
+#' @method plot BIF
+#' @export
 plot.BIF <- function(x, y="networks", ...) {
   x$plot(y, ...)
 }
 
 BIF$set("public", "plot", function(type="networks", ...) {
   if (type=="networks") {
-    return(invisible(starplotBIFnetworks(private$data)))
+    return(invisible(starplotBIFnetworks(private$data, ...)))
   } else {
-    return(invisible(starplotBIFcontrasts(private$data)))
+    return(invisible(starplotBIFcontrasts(private$data, ...)))
   }
 })
 
@@ -86,7 +93,8 @@ plot.NPAModules <- function(
   ...
   ) {
    return(invisible(plotNPAmodulesGlobal(
-     npmodules = x
+     npmodules = x,
+     ...
      #main = main,
      #useLayout = useLayout,
      #title = title,
