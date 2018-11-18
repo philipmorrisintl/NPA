dd3f237af1c282b19ff842e4ffe359a981b170fb
Sylvain Gubian <sylvain.gubian@pmi.com>
2018-11-19 00:22:13 +0100

    Reformatted and adde missing mypanel function

--- R/colorscale.R
+++ R/colorscale.R
@@ -16,36 +16,127 @@
 #' @param maxx Maximum value.
 #'
 #' @return A vector of colors.
-colorscale <- function(x, col1 = "navy", col2 = "blue", col3 = "white", col4 = "orange",
-    col5 = "red3", signed = FALSE, minx = min(x, na.rm = TRUE), maxx = max(x, na.rm = TRUE)) {
+colorscale <-
+  function(x,
+           col1 = "navy",
+           col2 = "blue",
+           col3 = "white",
+           col4 = "orange",
+           col5 = "red3",
+           signed = FALSE,
+           minx = min(x, na.rm = TRUE),
+           maxx = max(x, na.rm = TRUE)) {
     if (length(col1) == 5) {
-        col2 <- col1[2]
-        col3 <- col1[3]
-        col4 <- col1[4]
-        col5 <- col1[5]
-        col1 <- col1[1]
+      col2 <- col1[2]
+      col3 <- col1[3]
+      col4 <- col1[4]
+      col5 <- col1[5]
+      col1 <- col1[1]
     }
     # if (signed == TRUE & (min(x, na.rm = TRUE) > 0 | max(x, na.rm = TRUE) < 0)) {
-        # warning("No real signed version possible")
+    # warning("No real signed version possible")
     # }
     if (signed == TRUE) {
-        cole <- rep(NA, length(x))
-        if (length(cole[x < 0 & !is.na(x)]) > 0) {
-            cole[x < 0 & !is.na(x)] <- colorscale0(c(x[x < 0 & !is.na(x)], 0), col1,
-                col2, col3, col4 = NULL, col5 = NULL, minx = minx, maxx = 0)[-length(c(x[x <
-                0 & !is.na(x)], 0))]
-        }
-        if (length(cole[x >= 0 & !is.na(x)]) > 0) {
-            cole[x >= 0 & !is.na(x)] <- colorscale0(c(0, x[x >= 0 & !is.na(x)]),
-                col3, col4, col5, col4 = NULL, col5 = NULL, minx = 0, maxx = maxx)[-1]
-        }
+      cole <- rep(NA, length(x))
+      if (length(cole[x < 0 & !is.na(x)]) > 0) {
+        cole[x < 0 &
+               !is.na(x)] <- colorscale0(
+                 c(x[x < 0 & !is.na(x)], 0),
+                 col1,
+                 col2,
+                 col3,
+                 col4 = NULL,
+                 col5 = NULL,
+                 minx = minx,
+                 maxx = 0
+               )[-length(c(x[x <
+                               0 & !is.na(x)], 0))]
+      }
+      if (length(cole[x >= 0 & !is.na(x)]) > 0) {
+        cole[x >= 0 & !is.na(x)] <- colorscale0(
+          c(0, x[x >= 0 & !is.na(x)]),
+          col3,
+          col4,
+          col5,
+          col4 = NULL,
+          col5 = NULL,
+          minx = 0,
+          maxx = maxx
+        )[-1]
+      }
     }
     if (signed == FALSE) {
-        cole <- colorscale0(x, col1, col2, col3, col4, col5, minx = minx, maxx = maxx)
+      cole <-
+        colorscale0(x,
+                    col1,
+                    col2,
+                    col3,
+                    col4,
+                    col5,
+                    minx = minx,
+                    maxx = maxx)
     }
     return(cole)
-}
+  }
 
+#' Generate color panels by using 5 colors.
+#' @param n An \code{integer}.
+#' @param col1 A \code{character} string for color name.
+#' @param col2 A \code{character} string for color name.
+#' @param col3 A \code{character} string for color name.
+#' @param col4 A \code{character} string for color name.
+#' @param col5 A \code{character} string for color name.
+#' @return A \code{character} \code{vector} of colors.
+#' @importFrom grDevices rgb
+mypanel <- function(n,
+                    col1 = "navy",
+                    col2 = "blue",
+                    col3 = "white",
+                    col4 = "orange",
+                    col5 = "red3") {
+  if (length(col1) == 5) {
+    col2 <- col1[2]
+    col3 <- col1[3]
+    col4 <- col1[4]
+    col5 <- col1[5]
+    col1 <- col1[1]
+  }
+  col1 <- col2rgb(col1)
+  col2 <- col2rgb(col2)
+  col3 <- col2rgb(col3)
+  col4 <- col2rgb(col4)
+  col5 <- col2rgb(col5)
+  step <- floor(n / 4)
+  red <-
+    c(
+      seq(col1[1, 1], col2[1, 1], length = step),
+      seq(col2[1, 1], col3[1,
+                           1], length = step),
+      seq(col3[1, 1], col4[1, 1], length = step),
+      seq(col4[1,
+               1], col5[1, 1], length = n - 3 * step)
+    ) / 255
+  green <-
+    c(
+      seq(col1[3, 1], col2[3, 1], length = step),
+      seq(col2[3, 1], col3[3,
+                           1], length = step),
+      seq(col3[3, 1], col4[3, 1], length = step),
+      seq(col4[3,
+               1], col5[3, 1], length = n - 3 * step)
+    ) / 255
+  blue <-
+    c(
+      seq(col1[2, 1], col2[2, 1], length = step),
+      seq(col2[2, 1], col3[2,
+                           1], length = step),
+      seq(col3[2, 1], col4[2, 1], length = step),
+      seq(col4[2,
+               1], col5[2, 1], length = n - 3 * step)
+    ) / 255
+  col0 <- rgb(red, blue, green)
+  return(col0)
+}
 
 #' Creates a color scale (base function).
 #'
@@ -60,16 +151,25 @@ colorscale <- function(x, col1 = "navy", col2 = "blue", col3 = "white", col4 = "
 #' @param maxx Maximum value.
 #'
 #' @return Vector of colors
-colorscale0 <- function(x, col1 = "navy", col2 = "blue", col3 = "white", col4 = "orange",
-    col5 = "red3", minx = min(x, na.rm = TRUE), maxx = max(x, na.rm = TRUE)) {
+colorscale0 <-
+  function(x,
+           col1 = "navy",
+           col2 = "blue",
+           col3 = "white",
+           col4 = "orange",
+           col5 = "red3",
+           minx = min(x, na.rm = TRUE),
+           maxx = max(x, na.rm = TRUE)) {
     x <- c(minx, maxx, x)
     if (!is.null(col4) & !is.null(col5))
-        color0 <- mypanel(10001, col1, col2, col3, col4, col5)
+      color0 <- mypanel(10001, col1, col2, col3, col4, col5)
     if (is.null(col4) | is.null(col5)) {
-        color0 <- colorpanel(10001, col1, col2, col3)
+      color0 <- colorpanel(10001, col1, col2, col3)
     }
-    color0 <- color0[findInterval(x, seq(minx - 1e-06, maxx + 1e-06, length.out = 10001),
-        all.inside = TRUE)]
+    color0 <-
+      color0[findInterval(x,
+                          seq(minx - 1e-06, maxx + 1e-06, length.out = 10001),
+                          all.inside = TRUE)]
     color0 <- color0[-c(1:2)]
     return(color0)
-}
+  }
