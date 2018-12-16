affa9763e27f619fd01d8cb39cfab34bd37dd95a
Sylvain Gubian <sylvain.gubian@pmi.com>
2018-12-16 18:59:20 +0100

    Added missing npa_example file in git, updated DESCRIPTION file for supraHex (thanks to @ddomingof

---
+++ R/npa_example.R
@@ -0,0 +1,12 @@
+#####################################################################
+## Copyright 2018 Philip Morris Products, S.A.
+## Quai Jeanrenaud 5, 2000 Neuchatel, Switzerland
+#####################################################################
+
+#' Computed NPA object example
+#'
+#' This R6 NPA class object has been computed using COPD1 dataset
+#' example and a Mus musculs, CFA family, Apoptosis network.
+#' 
+#' @format A R6 NPA class object example.
+"npa_example"
